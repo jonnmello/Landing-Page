104b3cf1c56827b2138e56af9ce92919b87a6a9c
Jonatas Melo <jonnmello@gmail.com>
2022-09-26 18:50:23 -0300

    Formulario erros

--- frontend/src/components/Forms/Forms.tsx
+++ frontend/src/components/Forms/Forms.tsx
@@ -1,61 +1,102 @@
 import { useState } from 'react';
 import './styles.css';
 import emailjs from '@emailjs/browser';
+import { useForm } from "react-hook-form";
+
+
+type FormData = {
+  name: string;
+  message: string;
+  phone: string;
+  email: string;
+
+}
+
 
 const Forms = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [messages, setMessage] = useState('')
-  const [phone, setPhone] = useState('')
 
-  function sendEmail(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const [hasError, setHasError] = useState(false);
+
+  const onSubmit = (formData: FormData) => {
 
     const templateParams = {
-      from_name: name,
-      messages: messages,
-      email: email,
-      phone: phone
+      from_name: formData.name,
+      messages: formData.message,
+      email: formData.email,
+      phone: formData.phone
 
     }
-    emailjs.send("service_w2ob9yt", "template_uogzr2d", templateParams, "S7jqO9VwCuTP4RWQg");
-  }
+
+    if (formData.email !== '') {
+      emailjs.send("service_w2ob9yt", "template_uogzr2d", templateParams, "S7jqO9VwCuTP4RWQg")
+        .then((response) => {
+          setHasError(false);
+        })
+        .catch((error) => {
+          setHasError(true);
+          console.log('Erro', error);
+        });
+    } else {
+      setHasError(true);
+    }
+
+  };
 
 
 
   return (
     <div className="form-container">
       <h1 className="form-title">Contato</h1>
-
-      <form className="form" onSubmit={sendEmail}>
+      <span>Não encontrou sua dúvida? Entre em contato! </span>
+      {hasError && (
+        <div className="alert alert-danger">
+          Erro preenchimento
+        </div>
+      )}
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <input
-          className="input"
+          {...register("name")}
+          className="form-control input"
+          name="name"
           type="text"
           placeholder="Digite seu nome"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
+
         />
-        <input
-          className="input"
-          type="text"
-          placeholder="Digite seu telefone"
-          onChange={(e) => setPhone(e.target.value)}
-          value={phone}
-        />
+        <div className="invalid-feedback d-block">{errors.phone?.message}</div>
 
         <input
-          className="input"
+          {...register("phone", {
+            required: 'Campo obrigatório'
+          })}
+          className={`form-control input ${errors.phone ? 'is-invalid' : ''}`}
+          name="phone"
           type="text"
+          placeholder="Digite seu telefone"
+
+        />
+        <div className="invalid-feedback d-block">{errors.email?.message}</div>
+        <input
+          {...register("email", {
+            required: 'Campo obrigatório',
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: 'Email inválido'
+            }
+          })}
+          className={`form-control input ${errors.email ? 'is-invalid' : ''}`}
+          type="text"
+          name="email"
           placeholder="Digite seu email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
+
         />
 
         <textarea
-          className="textarea"
+          {...register("message")}
+          className="form-control textarea"
+          name="message"
           placeholder="Digite sua mensagem..."
-          onChange={(e) => setMessage(e.target.value)}
-          value={messages}
+
         />
 
         <input className="button" type="submit" value="Enviar" />
