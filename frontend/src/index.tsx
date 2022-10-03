5e77d64ea8f638bef79382cfeeee14a3ab4d39bf
Jonatas Melo <jonnmello@gmail.com>
2022-10-03 17:56:15 -0300

    testess

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import './index.css';
 
@@ -8,4 +9,4 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById("root")
-)
+);
