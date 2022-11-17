e34abd6626583e6d41595322194758ca3c55840a
Jonatas Melo <jonnmello@gmail.com>
2022-11-17 18:53:15 -0300

    orcamento

--- frontend/src/components/Services/index.tsx
+++ frontend/src/components/Services/index.tsx
@@ -1,7 +1,7 @@
 import './styles.css';
-import Img1 from "../../assets/img/serv1.fw.png";
-import Img2 from "../../assets/img/serv2.fw.png";
-import Img3 from "../../assets/img/serv3.fw.png";
+import construcaoImg1 from "../../assets/img/contrucao-projeto.png";
+import reformaImg2 from "../../assets/img/reforma-arquitetonico-residencia.png";
+import interioresImg3 from "../../assets/img/interiores-projetos-decoracao.png";
 import ServicesCard from './ServicesCard';
 import { useInView } from 'react-intersection-observer';
 import { motion } from 'framer-motion';
@@ -33,19 +33,19 @@ const Services = () => {
   const serviceCardT = [
     {
       title: 'Construção',
-      img: Img1,
+      img: construcaoImg1,
       span: 'Projetos inteligentes para o seu negócio. Projetamos edifícios e estruturas de uso comercial, com o objetivo criar ambientes atrativos e funcionais para melhorar a experiência de compra dos consumidores.',
       link: 'https://docs.google.com/forms/d/e/1FAIpQLSe44VaIEZypA8uXB1pVSSgxRE2gHxnp6KFw9aYAENgleqe2Kg/viewform'
     },
     {
       title: 'Interiores',
-      img: Img2,
+      img: interioresImg3,
       span: 'Projetos bem solucionados e criativos para sua casa. Alinhamos custo, benefício, estética e funcionalidade para os ambientes projetados, de acordo com as necessidades e desejos apresentados pelo cliente.',
       link: 'https://docs.google.com/forms/d/e/1FAIpQLSchctltvQQUoNZQK5wdibhsvElqRZhel7Co0cIGbhm1wVm_zQ/viewform'
     },
     {
       title: 'Reforma',
-      img: Img3,
+      img: reformaImg2,
       span: 'Projetar ambientes através da arte, decoração, psicologia das cores e funcionalidade dos espaços. Estudamos, planejamos e projetamos ambientes internos do seu negócio e do seu lar.',
       link: 'https://docs.google.com/forms/d/e/1FAIpQLScyEZKIhv3xQfktNcCZUdUZ1BlO2pUvsRVxeyBdmxSVXXYH8Q/viewform'
     }
