10bcafad8d372d76aed6c1be8fb0730e40fa527e
Jonatas Melo <jonnmello@gmail.com>
2023-04-28 11:32:41 -0300

    cont

--- frontend/src/components/Services/ServicesCard/index.tsx
+++ frontend/src/components/Services/ServicesCard/index.tsx
@@ -9,12 +9,17 @@ type Props = {
   link2: string
 }
 
+let numero = 0;
 
 const ServicesCard = ({ title, img, span, link, link2, span2 }: Props) => {
 
+  function aumentar() {
+    numero++
+    console.log(numero)
+  }
+
 
   return (
-
 
 
     <div className="services-planinfocontainer">
@@ -28,7 +33,7 @@ const ServicesCard = ({ title, img, span, link, link2, span2 }: Props) => {
         <a href={link} target="_parent"> <motion.button
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}>SAIBA MAIS</motion.button></a>
-        <a className="button1" href={link2} target="_blank"> <motion.button
+        <a className="button1" href={link2} onClick={aumentar} target="_blank"> <motion.button
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}>ORÇAMENTO</motion.button></a>
       </div>
